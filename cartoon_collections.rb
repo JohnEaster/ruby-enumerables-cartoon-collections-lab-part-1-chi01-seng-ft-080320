5014eecff90325bad0d2b8adc51fde3215ec5781
John Easter <john@owltestprep.com>
2020-07-17 14:59:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,18 @@
-def greet_characters(array)
+
+
+def greet_characters(array = ["Dopey", "Grumpy", "Bashful"])
+  array.each do |greet_characters|
+    puts "Hello #{greet_characters}!"
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
 end
 
-def list_dwarves(array)
+def list_dwarves(array = ["Dopey", "Grumpy", "Bashful", "Doc", "Sneezy", "Sleepy", "Happy"])
+  array.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
